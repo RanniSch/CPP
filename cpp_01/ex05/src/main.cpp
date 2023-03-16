4ce1b647e591dd72aa1906c3377bffd9be1e4d70
rschlott <rschlott@student.42wolfsburg.de>
2023-03-16 19:58:21 +0100

    ex05

---
+++ cpp_01/ex05/src/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rschlott <rschlott@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/16 19:47:07 by rschlott          #+#    #+#             */
+/*   Updated: 2023/03/16 19:57:50 by rschlott         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+#include <iostream>
+
+int main(int argc, char **argv) 
+{
+	Harl *harl;
+
+	harl = new Harl;
+    if (argc == 1)
+    {
+        std::cout << "Wrong input!" << std::endl <<
+        "Write: ./harl with minimum one level comment!" << std::endl;
+    }
+
+	delete harl;
+
+	return (0);
+}
