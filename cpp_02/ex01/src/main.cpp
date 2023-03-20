4655cc119e4e66ce7ed05d884ed51b5b8597b288
rschlott <rschlott@student.42wolfsburg.de>
2023-03-20 20:10:33 +0100

    ex00

--- cpp_02/ex01/src/main.cpp
+++ cpp_02/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rschlott <rschlott@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 22:06:04 by rschlott          #+#    #+#             */
-/*   Updated: 2023/03/20 19:50:47 by rschlott         ###   ########.fr       */
+/*   Updated: 2023/03/20 20:09:01 by rschlott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,22 @@
 
 int main(void)
 {
-    Fixed a;
-    Fixed b(a);
-    Fixed c;
+    Fixed       a;
+    Fixed const b( 10 );
+    Fixed const c( 42.42f );
+    Fixed const d( b );
+    
+    a = Fixed( 1234.4321f );
+    
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
 
-    c = b;
-
-    std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
-	return 0;
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+    
+    return 0;
 }
